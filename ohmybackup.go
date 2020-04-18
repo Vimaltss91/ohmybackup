605e393523eaed9264227d9cc2b839ef39402a06
Vimaltss91 <vimaltss91@gmail.com>
2020-04-18 14:11:09 +0530

    once more
    once mroe

--- ohmybackup.go
+++ ohmybackup.go
@@ -14,9 +14,9 @@ import (
 var userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_1 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A470a Safari/9537.53"
 
 // Path and Files
-var pathF = "/root/Downloads/wordlist/files"
-var extensions = "extensions.txt"
-var files = "files.txt"
+var pathF = ""
+var extensions = "/root/Downloads/wordlist/files/extensions.txt"
+var files = "/root/Downloads/wordlist/files/files.txt"
 var folders = "/root/Downloads/wordlist/files/folders.txt"
 var foundedFolders []string
 
